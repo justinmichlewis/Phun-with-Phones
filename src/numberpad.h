7719465d0de1aaf39b634ab2fa6b2df41a52566b
Justin Lewis <justinmichlewis@gmail.com>
2024-10-23 17:17:16 -0700

    updated code for new pin assignment

--- src/numberpad.h
+++ src/numberpad.h
@@ -1,19 +1,19 @@
 #include <Keypad.h>
 #include <Arduino.h>
 
-const char number_of_rows = 4;
-const char number_of_columns = 4;
-
-byte row_pins[number_of_rows] = {2, 3, 4, 5};
-byte column_pins[number_of_columns] = {6, 7, 8, 9};
-
-char key_array[number_of_rows][number_of_columns] = {
+// KEYPAD
+const byte ROWS = 4; // four rows
+const byte COLS = 4; // four columns
+char keys[ROWS][COLS] = {
     {'1', '2', '3', 'A'},
     {'4', '5', '6', 'B'},
     {'7', '8', '9', 'C'},
     {'*', '0', '#', 'D'}};
 
-Keypad k = Keypad(makeKeymap(key_array), row_pins, column_pins, number_of_rows, number_of_columns);
+byte rowPins[COLS] = {A3, A2, A1, A0}; // connect to the row pinouts of the keypad
+byte colPins[ROWS] = {7, 6, 5, 4};     // connect to the column pinouts of the keypad
+
+Keypad k = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 char getKeyPressed()
 {
